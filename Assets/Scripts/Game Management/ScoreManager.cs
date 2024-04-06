93d298ec540a0aa0a1752acbf477aa99d2d7298f
Matszak <mtracz2@edu.cdv.pl>
2024-04-06 18:08:37 +0200

    Score Manager - Update

--- Assets/Scripts/Game Management/ScoreManager.cs
+++ Assets/Scripts/Game Management/ScoreManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
@@ -13,4 +15,21 @@ public class ScoreManager : MonoBehaviour
 
     public int team1Score;
     public int team2Score;
+
+    [SerializeField]
+    private Text _scoreText1;
+    private Text _scoreText2;
+
+
+    private void Start()
+    {
+        _scoreText1.text = 0.ToString();
+        _scoreText2.text = 0.ToString();
+    }
+
+    public void Update()
+    {
+        _scoreText1.text = team1Score.ToString();
+        _scoreText2.text = team2Score.ToString();
+    }
 }
