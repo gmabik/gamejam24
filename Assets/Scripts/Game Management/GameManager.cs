8bb7062bb6bb1d0a58a667a74aa7a057d0cfe884
Vlad <vladik11012006@gmail.com>
2024-04-06 19:56:24 +0200

    small fast fix

--- Assets/Scripts/Game Management/GameManager.cs
+++ Assets/Scripts/Game Management/GameManager.cs
@@ -17,14 +17,14 @@ public class GameManager : MonoBehaviour
         Team1, Team2
     }
 
-    public GameObject ball {  get; private set; }
+    public GameObject Ball {  get; private set; }
 
     //public static int maxPlayers;
     [SerializeField] private GameObject[] team1Players;
     [SerializeField] private GameObject[] team2Players;
 
-    [SerializeField] private GameObject team1Goal;
-    [SerializeField] private GameObject team2Goal;
+    public GameObject team1Goal;
+    public GameObject team2Goal;
 
     public bool checkIfTheSameTeam(GameObject instigator, GameObject otherObject)
         => (team1Players.Contains(instigator) && team1Players.Contains(otherObject)) || (team2Players.Contains(instigator) && team2Players.Contains(otherObject));
