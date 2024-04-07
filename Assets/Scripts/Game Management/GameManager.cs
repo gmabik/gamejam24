a3534643d8935a1d8bc92d63defdb678dd04f921
Vlad <vladik11012006@gmail.com>
2024-04-07 02:09:04 +0200

    bot role system done

--- Assets/Scripts/Game Management/GameManager.cs
+++ Assets/Scripts/Game Management/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using UnityEngine;
 
@@ -17,7 +18,13 @@ public class GameManager : MonoBehaviour
         Team1, Team2
     }
 
-    [field: SerializeField] public GameObject Ball {  get; private set; }
+    public enum BotRole
+    {
+        Attacker,
+        Defender
+    }
+
+    [field: SerializeField] public GameObject Ball { get; private set; }
 
     //public static int maxPlayers;
     [SerializeField] private GameObject[] team1Players;
@@ -37,4 +44,47 @@ public class GameManager : MonoBehaviour
     public GameObject GetEnemyGoal(GameObject instigator)
         => team2Players.Contains(instigator) ? team1Goal : team2Goal;
 
+    private void RoleManagement(GameObject[] team)
+    {
+        bool teamHasPlayer = false;
+        foreach (GameObject player in team)
+        {
+            if (player.GetComponent<PlayerMovement>() != null)
+            {
+                teamHasPlayer = true;
+                break;
+            }
+        }
+
+        var sorted = team.OrderBy(item => Vector3.Distance(item.transform.position, Ball.transform.position)).ToArray();
+
+        int attackersAmountNeeded = teamHasPlayer ? 1 : 2;
+        int attackersAmountActual = 0;
+
+        int y = 0;
+        for (int i = 0; i < 2; i++)
+        {
+            y++;
+            if (sorted[i].TryGetComponent<AiScript>(out AiScript ai))
+            {
+                ai.role = BotRole.Attacker;
+                attackersAmountActual++;
+            }
+            if (attackersAmountActual == attackersAmountNeeded) break;
+        }
+
+        for (int i = y; i < sorted.Length; i++)
+        {
+            if (sorted[i].TryGetComponent<AiScript>(out AiScript ai))
+            {
+                ai.role = BotRole.Defender;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        RoleManagement(team1Players);
+        RoleManagement(team2Players);
+    }
 }
